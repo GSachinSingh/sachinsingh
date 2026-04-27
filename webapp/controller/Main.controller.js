1b2ab0350222ddce08d885585188407d04696410
GSachinSingh <gsachin.developer@gmail.com>
2026-04-27 22:07:00 +0530

    Initial Commit

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,9 +1,18 @@
-sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController, MessageBox) {
-	"use strict";
+sap.ui.define(
+	["./BaseController", "sap/m/MessageBox"],
+	function (BaseController, MessageBox) {
+		"use strict";
 
-	return BaseController.extend("sachinsingh.controller.Main", {
-		sayHello: function () {
-			MessageBox.show("Hello World!");
-		}
-	});
-});
+		return BaseController.extend("sachinsingh.controller.Main", {
+			onInit: function () {
+				this.oRouter = this.getOwnerComponent().getRouter();
+			},
+			onLinkedinPress: function () {
+				window.open(
+					"https://www.linkedin.com/in/sachin-singh-a6795b340",
+					"_blank",
+				);
+			},
+		});
+	},
+);
