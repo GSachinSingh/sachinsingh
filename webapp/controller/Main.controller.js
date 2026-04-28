700d12e41e735e2fab9181755304efb26fef2555
GSachinSingh <gsachin.developer@gmail.com>
2026-04-28 12:55:18 +0530

    Changed Exp Date

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -5,8 +5,26 @@ sap.ui.define(
 
 		return BaseController.extend("sachinsingh.controller.Main", {
 			onInit: function () {
-				this.oRouter = this.getOwnerComponent().getRouter();
+				// Example Usage:
+				const result = this.calculateExperience("2022-06-20");
+				this.aboutMe = `Dynamic and results-oriented Senior Technical Analyst with over ${result} years `;
+				let aboutText = this.getView().byId("aboutMe").getText();
+				this.getView()
+					.byId("aboutMe")
+					.setText(this.aboutMe + aboutText);
+				this.getView().byId("expsum").setProperty("count", result);
 			},
+			// Controller.js
+			calculateExperience: function (startDateStr) {
+				const startDate = new Date(startDateStr); // from model
+				const currentDate = new Date(); // always "now"
+
+				const diffInMs = currentDate - startDate;
+				const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365.25);
+
+				return diffInYears.toFixed(1); // e.g. "3.5"
+			},
+
 			onLinkedinPress: function () {
 				window.open(
 					"https://www.linkedin.com/in/sachin-singh-a6795b340",
